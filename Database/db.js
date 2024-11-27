ede177c13d2b8717546ed7a49228e2cdd38f7541
ibouibro <45498498+ibouibro@users.noreply.github.com>
2024-11-27 10:59:07 +0000

    Update db.js

--- Database/db.js
+++ Database/db.js
@@ -2,15 +2,20 @@ const mysql = require("mysql");
 
 
 // Create a connection to the database
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
   host: "sql7.freemysqlhosting.net",
   user: "sql7746347",
   password: "72gjwXK48L",
-  database: "sql7746347",
-  connectionLimit : 10
+  database: "sql7746347"
 });
 
 // open the MySQL connection
-
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion :', err.stack);
+    return;
+  }
+  console.log('Connecté à la base de données avec ID :', connection.threadId);
+});
 
 module.exports = connection;
